0c71dc9a8c2514534c32acd3f4cce37deac41693
Torstein Heltne Hovde <torstein.hovde@gmail.com>
2021-02-19 11:43:32 +0100

    fixed feasibilitycheck

--- feasibilityChecker.py
+++ feasibilityChecker.py
@@ -83,39 +83,40 @@ class feasibilityChecker():
             print("r: ",r)
             data = r.json()
 
-            data_pool = [data['results']['bindings'][0]['backHeightMax']['value'],\
-                        data['results']['bindings'][0]['backHeightMin']['value'],\
+            data_pool_customer = [data['results']['bindings'][0]['backHeight']['value'],\
                         data['results']['bindings'][0]['chairColor']['value'],\
                         data['results']['bindings'][0]['chairMaterial']['value'],\
-                        data['results']['bindings'][0]['legLengthMax']['value'],\
-                        data['results']['bindings'][0]['legLengthMin']['value'],\
-                        data['results']['bindings'][0]['legSideMax']['value'],\
-                        data['results']['bindings'][0]['legSideMin']['value'],\
+                        data['results']['bindings'][0]['legLength']['value'],\
+                        data['results']['bindings'][0]['legSide']['value'],\
                         data['results']['bindings'][0]['shapeMaterial']['value'],\
                         data['results']['bindings'][0]['backShape']['value'],\
-                        data['results']['bindings'][0]['seatSideMax']['value'],\
-                        data['results']['bindings'][0]['seatSideMin']['value']]
+                        data['results']['bindings'][0]['seatSide']['value']]
             
-            return data_pool
+            return data_pool_customer
 
     def feasibilityCheck(self):
-        """
-        # we agree that this should be in the dfaServer.py?
         #for-loop with list for expandability and KBE-friendly
-        production_intz_param = [leg_length1, leg_side1, seat_side1, back_height1, chair_color, back_shape_material1, chair_material1, number_chair1]
+
+        manufaqConstrains = s.retrieveManufaqConstrains()
+        production_intz_param = s.retrieveCustomerData()
+
         flagOK = False
-        if(production_intz_param[0] > leg_lengthLow) and (production_intz_param[0] < leg_lengthUp):
-            if (production_intz_param[1] > leg_sideLow) and (production_intz_param[1] < leg_sideUp):
-                if (production_intz_param[2] > seat_sideLow) and (production_intz_param[2] < leg_sideUp):
-                    if (production_intz_param[3] > back_heightLow) and (production_intz_param[3]< back_heightUp):
-                        if production_intz_param[4] in chair_color:
-                            if production_intz_param[5] in back_shape_material:
-                                if production_intz_param[6] in chair_material:
+        #arrangement of data_pool [backHeightMax,backHeightMin,chairColor,chairMaterial
+            # legLengthMax,legLengthMin,legSideMax,legSideMin,shapeMaterial,backShape
+            # seatSideMax,seatSideMin]
+
+        #The indexes is a mess but its correct.
+        if(production_intz_param[0] > manufaqConstrains[1]) and (production_intz_param[0] < manufaqConstrains[0]):
+            if (production_intz_param[3] > manufaqConstrains[5]) and (production_intz_param[3] < manufaqConstrains[4]):
+                if (production_intz_param[4] > manufaqConstrains[7]) and (production_intz_param[2] < manufaqConstrains[6]):
+                    if (production_intz_param[7] > manufaqConstrains[11]) and (production_intz_param[3] < manufaqConstrains[10]):
+                        if production_intz_param[1] in manufaqConstrains[2]:
+                            if production_intz_param[5] in manufaqConstrains[8]:
+                                if production_intz_param[2] in manufaqConstrains[3]:
                                     if materialCalculation(production_intz_param[7]):
-                                        s.wfile.write(bytes('OK','utf-8'))
-                                        print("The parameters are accepted")
+                                        return s.wfile.write(bytes('OK','utf-8'))
+                                        #print("The parameters are accepted")
                                         flagOK = True
         else:
-            s.wfile.write(bytes('Not OK', 'utf-8'))
-            print("The parameters given is not accepted")
-        """
+            return s.wfile.write(bytes('Not OK', 'utf-8'))
+            #print("The parameters given is not accepted")
